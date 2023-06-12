14e6d75e1d58bd58ed36031e9141af4aa65e8b81
samuelcaldas <samuel.caldas@gmail.com>
2023-06-12 01:56:16 -0300

    Add Asserts and TestRunner

---
+++ Assert.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using NinjaTrader.Cbi;
+
+namespace NinjaTrader.UnitTest
+{
+    public static class Assert
+    {
+        public static void AreEqual(object expected, object actual, string message = null)
+        {
+            if (!expected.Equals(actual))
+            {
+                Fail(message ?? $"AssertionError: {expected} != {actual}");
+            }
+        }
+
+        public static void IsTrue(bool condition, string message = null)
+        {
+            if (!condition)
+            {
+                Fail(message ?? "AssertionError: Condition is not true");
+            }
+        }
+
+        public static void IsFalse(bool condition, string message = null)
+        {
+            if (condition)
+            {
+                Fail(message ?? "AssertionError: Condition is not false");
+            }
+        }
+
+        // Outros métodos de assert podem ser adicionados aqui
+
+        private static void Fail(string message)
+        {
+            NinjaScript.Log(message, LogLevel.Error);
+            throw new Exception(message);
+        }
+    }
+
+}
---
+++ TestRunner.cs
@@ -0,0 +1,10 @@
+﻿namespace NinjaTrader.UnitTest
+{
+    public static class TestRunner
+    {
+        public static void Run(TestSuite testSuite)
+        {
+            testSuite.Run();
+        }
+    }
+}
