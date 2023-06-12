b6cd02260164013057a6187e10ea4de33dded57c
samuelcaldas <samuel.caldas@gmail.com>
2023-06-12 02:08:23 -0300

    Add more Asserts

--- Assert.cs
+++ Assert.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using NinjaTrader.Cbi;
+using NinjaTrader.NinjaScript;
 
 namespace NinjaTrader.UnitTest
 {
@@ -10,6 +11,14 @@ namespace NinjaTrader.UnitTest
             if (!expected.Equals(actual))
             {
                 Fail(message ?? $"AssertionError: {expected} != {actual}");
+            }
+        }
+
+        public static void AreNotEqual(object expected, object actual, string message = null)
+        {
+            if (expected.Equals(actual))
+            {
+                Fail(message ?? $"AssertionError: {expected} == {actual}");
             }
         }
 
@@ -29,13 +38,76 @@ namespace NinjaTrader.UnitTest
             }
         }
 
+        public static void Is(object expected, object actual, string message = null)
+        {
+            if (!ReferenceEquals(expected, actual))
+            {
+                Fail(message ?? $"AssertionError: {expected} is not the same object as {actual}");
+            }
+        }
+
+        public static void IsNot(object expected, object actual, string message = null)
+        {
+            if (ReferenceEquals(expected, actual))
+            {
+                Fail(message ?? $"AssertionError: {expected} is the same object as {actual}");
+            }
+        }
+
+        public static void IsNone(object obj, string message = null)
+        {
+            if (obj != null)
+            {
+                Fail(message ?? $"AssertionError: {obj} is not None");
+            }
+        }
+
+        public static void IsNotNone(object obj, string message = null)
+        {
+            if (obj == null)
+            {
+                Fail(message ?? "AssertionError: object is None");
+            }
+        }
+
+        public static void IsIn(object item, object collection, string message = null)
+        {
+            if (!((System.Collections.IList)collection).Contains(item))
+            {
+                Fail(message ?? $"AssertionError: {item} not found in {collection}");
+            }
+        }
+
+        public static void IsNotIn(object item, object collection, string message = null)
+        {
+            if (((System.Collections.IList)collection).Contains(item))
+            {
+                Fail(message ?? $"AssertionError: {item} found in {collection}");
+            }
+        }
+
+        public static void IsInstance(object obj, Type type, string message = null)
+        {
+            if (!(type.IsInstanceOfType(obj)))
+            {
+                Fail(message ?? $"AssertionError: {obj} is not an instance of {type}");
+            }
+        }
+
+        public static void IsNotInstance(object obj, Type type, string message = null)
+        {
+            if (type.IsInstanceOfType(obj))
+            {
+                Fail(message ?? $"AssertionError: {obj} is an instance of {type}");
+            }
+        }
+
         // Outros métodos de assert podem ser adicionados aqui
 
         private static void Fail(string message)
         {
-            NinjaScript.Log(message, LogLevel.Error);
+            NinjaScript.NinjaScript.Log(message, LogLevel.Error);
             throw new Exception(message);
         }
     }
-
 }
