723960c19c885c2d1299f22dcec91e41a7d0604d
samuelcaldas <samuel.caldas@gmail.com>
2023-06-12 03:55:41 -0300

    Update TestResult Class

--- TestResult.cs
+++ TestResult.cs
@@ -4,15 +4,39 @@ namespace NinjaTrader.UnitTest
 {
     public class TestResult
     {
-        public int RunCount { get; set; }
-        public int FailCount { get; set; }
-        public List<string> Failures { get; set; }
+        public int TestsRun => successes + failures + errors;
 
-        public TestResult()
+        public int Successes => successes;
+
+        public int Failures => failures;
+
+        public int Errors => errors;
+
+        public List<(TestCase, string)> FailuresList => failuresList;
+
+        public List<(TestCase, string)> ErrorsList => errorsList;
+
+        internal void AddSuccess()
         {
-            RunCount = 0;
-            FailCount = 0;
-            Failures = new List<string>();
+            successes++;
         }
+
+        internal void AddFailure(TestCase testCase, string message)
+        {
+            failures++;
+            failuresList.Add((testCase, message));
+        }
+
+        internal void AddError(TestCase testCase, string message)
+        {
+            errors++;
+            errorsList.Add((testCase, message));
+        }
+
+        private int successes = 0;
+        private int failures = 0;
+        private int errors = 0;
+        private List<(TestCase, string)> failuresList = new List<(TestCase, string)>();
+        private List<(TestCase, string)> errorsList = new List<(TestCase, string)>();
     }
 }
