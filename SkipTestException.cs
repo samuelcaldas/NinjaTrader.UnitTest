849e5b38ccfba93fdadd26d42e06ac62d8f8f22c
samuelcaldas <samuel.caldas@gmail.com>
2023-06-12 04:18:06 -0300

    Add new classes

---
+++ SkipTestException.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace NinjaTrader.UnitTest
+{
+    public class SkipTestException : Exception
+    {
+        public SkipTestException(string message) : base(message) { }
+    }
+}
---
+++ SubTest.cs
@@ -0,0 +1,39 @@
+﻿using NinjaTrader.Cbi;
+using System;
+using System.Collections.Generic;
+
+namespace NinjaTrader.UnitTest
+{
+    public class SubTest : IDisposable
+    {
+        private TestCase testCase;
+        private string msg;
+        private Dictionary<string, object> parameters;
+        private bool success;
+
+        public SubTest(TestCase testCase, string msg = null, Dictionary<string, object> parameters = null)
+        {
+            this.testCase = testCase;
+            this.msg = msg;
+            this.parameters = parameters ?? new Dictionary<string, object>();
+            this.success = true;
+        }
+
+        public void Dispose()
+        {
+            if (success)
+            {
+                NinjaTrader.NinjaScript.NinjaScript.Log($"{testCase.GetType().Name} ({msg}) ... ok", LogLevel.Information);
+            }
+            else
+            {
+                NinjaTrader.NinjaScript.NinjaScript.Log($"{testCase.GetType().Name} ({msg}) ... FAIL", LogLevel.Error);
+            }
+        }
+
+        internal void SetSuccess(bool success)
+        {
+            this.success = success;
+        }
+    }
+}
